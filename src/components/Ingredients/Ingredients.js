a4699dc07b7e4593316a7bc78426162fe11f3388
sunadoi <y.sunadoi@gmail.com>
2020-05-04 21:03:54 +0900

    HTTP request by fetch()

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -8,9 +8,18 @@ const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients,
-      {id: Math.random().toString(), ...ingredient}
-    ]);
+    fetch('https://react-hooks-update-2ed1b.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [
+        ...prevIngredients,
+        {id: responseData.name, ...ingredient}
+      ]);
+    })
   }
 
   const removeIngredientHandler = ingId => {
