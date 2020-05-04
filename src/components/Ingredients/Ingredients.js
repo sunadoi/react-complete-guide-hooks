2006931c256dae9df7ea108bf895f121eade235f
sunadoi <y.sunadoi@gmail.com>
2020-05-04 19:39:49 +0900

    removeIngredientHandler

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,13 +7,23 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients,
+      {id: Math.random().toString(), ...ingredient}
+    ]);
+  }
+
+  const removeIngredientHandler = ingId => {
+    setUserIngredients(prevIngredients => prevIngredients.filter(ingredient => ingredient.id !== ingId))
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
       </section>
     </div>
   );
